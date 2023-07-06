4ec493dfc33be0967c0ee400222305711c831b29
nitish0104 <nitishdalvi1@gmail.com>
2023-07-06 10:25:37 +0530

    Form

--- src/pages/AdminPages/Main.jsx
+++ src/pages/AdminPages/Main.jsx
@@ -63,7 +63,7 @@ const Main = () => {
   };
 
   const handleEditClick = () => {
-    setData((prevData) => ({
+    setformState((prevData) => ({
       ...prevData,
     }));
     setisEditable(true);
